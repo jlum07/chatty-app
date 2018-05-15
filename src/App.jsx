7ae42b41d1c20626aaf4e8b30e1588f73aa54471
Justin <j.lum07@gmail.com>
2018-05-15 19:41:21 -0400

    🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,97 @@
 import React, {Component} from 'react';
+import NavBar from './NavBar.jsx';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
+
+// const messages = [
+//   {
+//     type: "incomingMessage",
+//     content: "I won't be impressed with technology until I can download food.",
+//     username: "Anonymous1"
+//   },
+//   {
+//     type: "incomingNotification",
+//     content: "Anonymous1 changed their name to nomnom",
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+//     username: "Anonymous2"
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "...",
+//     username: "nomnom"
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+//     username: "Anonymous2"
+//   },
+//   {
+//     type: "incomingMessage",
+//     content: "This isn't funny. You're not funny",
+//     username: "nomnom"
+//   },
+//   {
+//     type: "incomingNotification",
+//     content: "Anonymous2 changed their name to NotFunny",
+//   },
+// ];
+
+const appData = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ]
+};
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = appData;
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
+  onEnter(event) {
+
+    // console.log(testing);
+
+    if (event.key === "Enter") {
+      console.log(event.target.value);
+      console.log("ENTER!!!!!", event);
+      // console.log(username, message);
+    }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <React.Fragment>
+        <NavBar />
+        <MessageList messages={this.state.messages} />
+        <ChatBar currentUser={this.state.currentUser} onEnter={this.onEnter} />
+      </React.Fragment>
     );
   }
 }
---
+++ src/MessageList.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+
+  render() {
+
+    const messageList = this.props.messages.map((message, index) => {
+      return <Message message={message} key={index} />
+    });
+
+    return (
+      <main className="messages">
+        {messageList}
+      </main>
+    );
+  }
+}
+export default MessageList;
